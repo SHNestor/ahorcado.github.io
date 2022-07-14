56089be12b8ffa8fbd9dbaa406f66ddc35101666
nestor sanchez <nsh1831100515@gmail.com>
2022-07-13 23:32:29 -0500

    Falta agregar el logo y las fraces ganadoras

--- code.js
+++ code.js
@@ -1,14 +1,13 @@
-const wordContainer= document.getElementById('wordContainer');
-const startButton= document.getElementById('startButton');
-const usedLettersElement= document.getElementById('usedLetters');
+const wordContainer = document.getElementById('wordContainer');
+const startButton = document.getElementById('startButton');
+const usedLettersElement = document.getElementById('usedLetters');
 
-let canvas= document.getElementById('canvas');
-let ctx= canvas.getContext('2d');
-ctx.canvas.width= 0;
-ctx.canvas.height= 0;
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+ctx.canvas.width  = 0;
+ctx.canvas.height = 0;
 
-const bodyParts= 
-[
+const bodyParts = [
     [4,2,1,1],
     [4,3,1,2],
     [3,5,1,1],
@@ -22,39 +21,39 @@ let usedLetters;
 let mistakes;
 let hits;
 
-const addLetter= letter =>
-{
-    const petterElement= document.createElement('span');
-    petterElement.innerHTML= letter.toUpperCase();
-    usedLettersElement.appendChild(petterElement);
+const addLetter = letter => {
+    const letterElement = document.createElement('span');
+    letterElement.innerHTML = letter.toUpperCase();
+    usedLettersElement.appendChild(letterElement);
 }
 
-const addBodyPart= bodyPart =>
-{
-    ctx.fillStyle= '#fff';
+const addBodyPart = bodyPart => {
+    ctx.fillStyle = '#fff';
     ctx.fillRect(...bodyPart);
-}
+};
 
-const  wrongLetter= () =>
-{
+const wrongLetter = () => {
     addBodyPart(bodyParts[mistakes]);
     mistakes++;
     if(mistakes === bodyParts.length) endGame();
+   
 }
 
-const endGame= () =>
-{
+const endGame = () => {
     document.removeEventListener('keydown', letterEvent);
-    startButton.style.display= 'block';
+    startButton.style.display = 'block';
+    if(hits == selectedWord.length)
+    {
+        alert("You are winner");
+    }else{
+        alert("You are loser");
+    }
 }
 
-const correctLetter= letter =>
-{
-    const {children }= wordContainer;
-    for(let i=0; i<children.length; i++)
-    {
-        if(children[i].innerHTML === letter)
-        {
+const correctLetter = letter => {
+    const { children } =  wordContainer;
+    for(let i = 0; i < children.length; i++) {
+        if(children[i].innerHTML === letter) {
             children[i].classList.toggle('hidden');
             hits++;
         }
@@ -62,70 +61,61 @@ const correctLetter= letter =>
     if(hits === selectedWord.length) endGame();
 }
 
-const letterInput= letter => 
-{
-    if(selectedWord.includes(letter))
-    {
+const letterInput = letter => {
+    if(selectedWord.includes(letter)) {
         correctLetter(letter);
-    }else{
+    } else {
         wrongLetter();
     }
     addLetter(letter);
     usedLetters.push(letter);
-}
+};
 
-const letterEvent= event =>
-{
-    let newLetter= event.key.toUpperCase();
-    if(newLetter.match(/^[a-zñ]$/i) && !usedLetters.includes(newLetter))
-    {
+const letterEvent = event => {
+    let newLetter = event.key.toUpperCase();
+    if(newLetter.match(/^[a-zñ]$/i) && !usedLetters.includes(newLetter)) {
         letterInput(newLetter);
-    }
-}
+    };
+};
 
-const drawWord= () => 
-{
-    selectedWord.forEach(letter => 
-        {
-            const usedLettersElement= document.createElement('span');
-            usedLettersElement.innerHTML= letter.toUpperCase();
-            usedLettersElement.classList.add('letter');
-            usedLettersElement.classList.add('hidden');
-            wordContainer.appendChild(usedLettersElement);
-        })
-}
+const drawWord = () => {
+    selectedWord.forEach(letter => {
+        const letterElement = document.createElement('span');
+        letterElement.innerHTML = letter.toUpperCase();
+        letterElement.classList.add('letter');
+        letterElement.classList.add('hidden');
+        wordContainer.appendChild(letterElement);
+    });
+};
 
-const selectRandomWord= () =>
-{
-    let word= words[Math.floor((Math.random()*words.length))].toUpperCase();
-    selectedWord= word.split('');
-}
+const selectRandomWord = () => {
+    let word = words[Math.floor((Math.random() * words.length))].toUpperCase();
+    selectedWord = word.split('');
+};
 
-const drawHangMan= () =>
-{
-    ctx.canvas.width= 120;
-    ctx.canvas.height= 160;
+const drawHangMan = () => {
+    ctx.canvas.width  = 120;
+    ctx.canvas.height = 160;
     ctx.scale(20, 20);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle= '#d95d39';
+    ctx.fillStyle = '#d95d39';
     ctx.fillRect(0, 7, 4, 1);
     ctx.fillRect(1, 0, 1, 8);
     ctx.fillRect(2, 0, 3, 1);
     ctx.fillRect(4, 1, 1, 1);
-}
+};
 
-const starGame= () =>
-{
-    usedLetters= [];
-    mistakes= 0;
-    hits= 0;
-    wordContainer.innerHTML= '';
-    usedLetters.innerHTML= '';
-    startButton.style.display= 'none';
+const startGame = () => {
+    usedLetters = [];
+    mistakes = 0;
+    hits = 0;
+    wordContainer.innerHTML = '';
+    usedLettersElement.innerHTML = '';
+    startButton.style.display = 'none';
     drawHangMan();
     selectRandomWord();
     drawWord();
     document.addEventListener('keydown', letterEvent);
-}
+};
 
-startButton.addEventListener('click', starGame);
+startButton.addEventListener('click', startGame);
--- palabras.js
+++ palabras.js
@@ -1,1 +1,20 @@
-const words= ['hola', 'tomas', 'tomate', 'nestor', 'keyvin', 'zanahoria', 'lapiz'];
+const nuevaPalabra= document.querySelector("#palabraNueva");
+const Button = document.getElementById('ingresarP');
+
+const words= ['hola'];
+
+const newWord= () =>
+{
+    let mensaje= nuevaPalabra.value;   
+    if(mensaje.length > 0)
+    {
+       console.log(mensaje);
+       console.log(words.length);
+       words.push(mensaje);
+       console.log(words.length);
+       nuevaPalabra.value= " ";
+
+    }
+}
+
+Button.addEventListener('click', newWord);
